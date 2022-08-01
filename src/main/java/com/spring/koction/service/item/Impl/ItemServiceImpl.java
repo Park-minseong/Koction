353733205e3878682df629ab40f94d49c838fc6e
김광민 <kenny@gimgwangmin-ui-MacBookAir.local>
2022-08-02 02:35:20 +0900

    controller, service, serviceimpl

--- src/main/java/com/spring/koction/service/item/Impl/ItemServiceImpl.java
+++ src/main/java/com/spring/koction/service/item/Impl/ItemServiceImpl.java
@@ -1,5 +1,9 @@
 package com.spring.koction.service.item.Impl;
 
+import com.spring.koction.entity.ItemFile;
+import com.spring.koction.repository.ItemRepository;
+import com.spring.koction.repository.ItemqRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -7,12 +11,45 @@ import org.springframework.stereotype.Service;
 import com.spring.koction.entity.Item;
 import com.spring.koction.service.item.ItemService;
 
+import java.util.List;
+
 @Service
 public class ItemServiceImpl implements ItemService{
+	@Autowired
+	ItemRepository itemRepository;
+	@Autowired
+	ItemqRepository itemqRepository;
 
 	@Override
 	public Page<Item> getItemList(Item item, Pageable pageable) {
 		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int registerItem(Item item) {
+		itemRepository.save(item);
+		itemRepository.flush();
+		return item.getItemNo();
+	}
+
+	@Override
+	public Item getMyItem(int itemNo) {
+		return itemRepository.findById(itemNo).get();
+	}
+
+	@Override
+	public void updateItem(Item item) {
+		itemRepository.save(item);
+	}
+
+	@Override
+	public void registerItemFileList(List<ItemFile> fileList) {
+		System.out.println("hello world");
+	}
+
+	@Override
+	public List<ItemFile> getItemFileList(int itemNo) {
 		return null;
 	}
 
--- src/main/java/com/spring/koction/service/item/ItemService.java
+++ src/main/java/com/spring/koction/service/item/ItemService.java
@@ -1,12 +1,25 @@
 package com.spring.koction.service.item;
 
+import com.spring.koction.entity.ItemFile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.spring.koction.entity.Item;
 
+import java.util.List;
+
 public interface ItemService {
 
 	Page<Item> getItemList(Item item, Pageable pageable);
+
+	int registerItem(Item item);
+
+	Item getMyItem(int itemNo);
+
+	void updateItem(Item item);
+
+	void registerItemFileList(List<ItemFile> fileList);
+
+	List<ItemFile> getItemFileList(int itemNo);
 
 }
