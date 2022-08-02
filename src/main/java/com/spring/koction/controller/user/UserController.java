4b440a7638c038bef203984cee7e57dc766a8470
younjung kim <kyj931110@naver.com>
2022-08-02 11:37:43 +0900

    2022080201

--- src/main/java/com/spring/koction/controller/user/UserController.java
+++ src/main/java/com/spring/koction/controller/user/UserController.java
@@ -1,13 +1,25 @@
 package com.spring.koction.controller.user;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.koction.entity.User;
+import com.spring.koction.service.user.UserService;
+
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	UserService userService;
+
 	
 	//로그인 페이지 이동
 	@GetMapping("/login")
@@ -17,6 +29,21 @@ public class UserController {
 		return mv;
 	}
 	
+	@PostMapping("/login")
+	public String login(User user, HttpSession session) {
+		User loginUser = userService.idCheck(user.getUserId());
+				if(loginUser == null) {
+					return "idFail";
+				}else{
+					if(!loginUser.getUserPw().equals(user.getUserPw())) {
+				return "pwFail";
+			} else {
+				session.setAttribute("loginUser", loginUser);
+				return "loginSuccess";
+			}
+		}
+	
+	}
 	
 	
 	//회원가입 페이지 이동
@@ -27,4 +54,29 @@ public class UserController {
 		return mv;
 	}
 	
+	@GetMapping("/mypage")
+	public ModelAndView mypage() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/user/mypage.html");
+		return mv;
+	}
+	
+	@GetMapping("/logout")
+	public ModelAndView logout(HttpSession session) {
+		session.invalidate();
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/user/login.html");
+		return mv;
+	}
+	
+	//회원정보수정
+	@GetMapping("/modifyInfo")
+	public ModelAndView modifyInfo() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/user/modifyInfo.html");
+		return mv;
+	}
+	
+
+
 }
--- src/main/java/com/spring/koction/service/item/Impl/ItemServiceImpl.java
+++ src/main/java/com/spring/koction/service/item/Impl/ItemServiceImpl.java
@@ -1,11 +1,14 @@
 package com.spring.koction.service.item.Impl;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.spring.koction.entity.Item;
 import com.spring.koction.service.item.ItemService;
+
+
 
 @Service
 public class ItemServiceImpl implements ItemService{
